83793d1bd7b5d674fecb2fd9304040e92c7194ac
Felipe Menezes <felipe.menezes@supersim.com.br>
2022-09-12 22:14:31 -0300

    Estudo de spring data jpa

--- src/main/java/com/springbootexpert/SpringBootExpertApplication.java
+++ src/main/java/com/springbootexpert/SpringBootExpertApplication.java
@@ -1,27 +1,42 @@
 package com.springbootexpert;
 
 import com.springbootexpert.domain.entity.Cliente;
-import com.springbootexpert.domain.repositorio.Clientes;
+import com.springbootexpert.domain.entity.Pedido;
+import com.springbootexpert.domain.repository.Clientes;
+import com.springbootexpert.domain.repository.Pedidos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
 public class SpringBootExpertApplication {
 
 	@Bean
-	public CommandLineRunner init(@Autowired Clientes clientes) {
+	public CommandLineRunner init(
+			@Autowired Clientes clientes,
+			@Autowired Pedidos pedidos
+	) {
 		return args -> {
 			System.out.println("Salvando clientes");
-			clientes.save(new Cliente("Felipe"));
-			clientes.save(new Cliente("Outro cliente"));
+			Cliente fulano = new Cliente("Outro cliente");
+			clientes.save(fulano);
 
-			boolean existe = clientes.existByNome("Felipe");
-			System.out.println("Existe um cliente com o nome Felipe? " + existe);
+			Pedido p = new Pedido();
+			p.setCliente(fulano);
+			p.setDataPedido((LocalDate.now()));
+			p.setTotal(BigDecimal.valueOf(100));
+
+			pedidos.save(p);
+
+			Cliente cliente = clientes.findClienteFetchPedidos((fulano.getId()));
+			System.out.println(cliente);
+//			System.out.println(cliente.getPedidos());
 
 		};
 	}
--- src/main/java/com/springbootexpert/domain/entity/Cliente.java
+++ src/main/java/com/springbootexpert/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package com.springbootexpert.domain.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "cliente")
@@ -13,6 +14,9 @@ public class Cliente {
 
     @Column(name = "nome", length = 100)
     private String nome;
+
+    @OneToMany ( mappedBy = "cliente")
+    private Set<Pedido> pedidos;
 
     public Cliente() {
     }
@@ -49,4 +53,6 @@ public class Cliente {
                 ", nome='" + nome + '\'' +
                 '}';
     }
+
+
 }
